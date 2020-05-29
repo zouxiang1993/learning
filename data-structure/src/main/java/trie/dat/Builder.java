27d3cbe069d7e0765d436bb6c8d27f6d57fe4126
邹祥 <zouxiang@wljs.com>
2020-05-29 15:57:39 +0800

    Double Array Trie 2

--- data-structure/src/main/java/trie/dat/Builder.java
+++ data-structure/src/main/java/trie/dat/Builder.java
@@ -7,6 +7,9 @@ import java.util.Objects;
 
 /**
  * Description: Double Array Trie 的构建工具
+ * 参考:
+ * https://www.hankcs.com/program/java/%E5%8F%8C%E6%95%B0%E7%BB%84trie%E6%A0%91doublearraytriejava%E5%AE%9E%E7%8E%B0.html
+ * https://github.com/hankcs/HanLP/blob/1.x/src/main/java/com/hankcs/hanlp/collection/trie/DoubleArrayTrie.java
  *
  * @author zouxiang
  * @date 2020/5/28
@@ -21,7 +24,7 @@ public class Builder {
     private int base[];
     private int check[];
     /**
-     * base 和 check 的大小
+     * base 和 check 的大小  TODO: 记录的应该是被使用的最大的位置，用于最后缩容。 可以把这个变量删掉？
      */
     private int size;
 
@@ -29,10 +32,6 @@ public class Builder {
      * 下一次插入时从check[]中的哪个位置开始找。
      */
     private int nextCheckPos;
-    /**
-     * TODO: 删掉这个。直接用 base.length来替代
-     */
-    private int allocSize;
 
     /**
      * 唯一的构建方法
@@ -49,10 +48,9 @@ public class Builder {
 
         this.patterns = patterns;
         this.values = values;
-        allocSize = 0;
 
         // 给双数组一个初始的大小: 32个双字节
-        resize(65536 * 32);
+        ensureSize(65536 * 32);
 
         base[0] = 1;
         nextCheckPos = 0;
@@ -91,7 +89,7 @@ public class Builder {
             int cur = 0;
             if (pattern.length() != parent.depth) {
                 // Q: 为什么要加1？
-                // A: 在树中，code=0有特殊的含义 —— 表示一个字符串的结束。因此其他的code都必须大于等于1
+                // A: 在树中，code=0专门用来表示一个字符串的结束。因此其他的code都必须大于等于1
                 cur = (int) pattern.charAt(parent.depth) + 1;
             }
 
@@ -110,7 +108,7 @@ public class Builder {
                 newNode.left = i;
                 // 如果新增的不是第一个节点，那么上一个节点就已经构建完毕，设置一下它的right属性
                 if (children.size() != 0) {
-                    children.get(children.size() - 1).right = i;
+                    lastOf(children).right = i;
                 }
                 children.add(newNode);
             }
@@ -120,7 +118,7 @@ public class Builder {
 
         // 设置最右节点的right属性
         if (children.size() != 0) {
-            children.get(children.size() - 1).right = parent.right;
+            lastOf(children).right = parent.right;
         }
 
         return children;
@@ -129,37 +127,80 @@ public class Builder {
     /**
      * 插入节点
      *
-     * @param siblings 等待插入的兄弟节点
+     * @param siblings 一批等待插入的兄弟节点
      * @return 插入位置
      */
     private int insert(List<Node> siblings, BitSet used) {
+        int begin = findCheckPos(siblings, used);
+        used.set(begin);
+
+        // TODO: ...
+        if (size <= begin + lastOf(siblings).code + 1) {
+            size = begin + lastOf(siblings).code + 1;
+        }
+
+        for (int i = 0; i < siblings.size(); i++) {
+            check[begin + siblings.get(i).code] = begin;
+        }
+
+        for (int i = 0; i < siblings.size(); i++) {
+            List<Node> newChildren = createAllChildren(siblings.get(i));
+            if (newChildren.size() > 0) {
+                // 递归处理所有的子节点
+                int h = insert(newChildren, used);
+                base[begin + siblings.get(i).code] = h;
+            } else {
+                // 如果没有子节点，则说明是一个词的末尾且不为其他词的前缀
+                base[begin + siblings.get(i).code] = (values != null) ?
+                        (-values[siblings.get(i).left] - 1) : (-siblings.get(i).left - 1);
+
+                if (values != null && (-values[siblings.get(i).left] - 1) >= 0) {
+                    throw new RuntimeException("errorCode = -2 ???");
+                }
+            }
+        }
+        return begin;
+    }
+
+    /**
+     * 返回List中的最后一个节点
+     */
+    private Node lastOf(List<Node> nodes) {
+        return nodes.get(nodes.size() - 1);
+    }
+
+    private int findCheckPos(List<Node> siblings, BitSet used) {
+        boolean isFirst = true;
         int pos = Math.max(siblings.get(0).code + 1, nextCheckPos) - 1;
         int nonZeroNum = 0;
-        int first = 0;
 
-        if (allocSize <= pos)
-            resize(pos + 1);
+        // 一个简单的启发式判断条件：
+        // 如果从nextCheckPos到pos之间的空间已经占用了95%以上，那么再下次插入节点时，直接从pos位置处开始查找。
+        if (1.0 * nonZeroNum / (pos - nextCheckPos + 1) >= 0.95) {
+            nextCheckPos = pos;
+        }
 
         int begin;
-        outer:
         // 此循环体的目标是找出满足base[begin + a1...an]  == 0的n个空闲空间,a1...an是siblings中的n个节点
+        outer:
         while (true) {
             pos++;
 
-            if (allocSize <= pos)
-                resize(pos + 1);
+            if (check.length <= pos) {
+                ensureSize(pos + 1);
+            }
 
             if (check[pos] != 0) {
                 nonZeroNum++;
                 continue;
-            } else if (first == 0) {
+            } else if (isFirst) {
                 nextCheckPos = pos;
-                first = 1;
+                isFirst = false;
             }
 
             begin = pos - siblings.get(0).code; // 当前位置离第一个兄弟节点的距离
-            if (allocSize <= (begin + siblings.get(siblings.size() - 1).code)) {
-                resize(begin + siblings.get(siblings.size() - 1).code + Character.MAX_VALUE);
+            if (check.length <= begin + siblings.get(siblings.size() - 1).code) {
+                ensureSize(begin + siblings.get(siblings.size() - 1).code + Character.MAX_VALUE);
             }
 
             if (used.get(begin)) {
@@ -174,59 +215,29 @@ public class Builder {
 
             break;
         }
-
-        // 一个简单的启发式判断条件：
-        // 如果从nextCheckPos到pos之间的空间已经占用了95%以上，那么再下次插入节点时，直接从pos位置处开始查找。
-        if (1.0 * nonZeroNum / (pos - nextCheckPos + 1) >= 0.95) {
-            nextCheckPos = pos;
-        }
-
-        used.set(begin);
-
-        size = (size > begin + siblings.get(siblings.size() - 1).code + 1) ? size
-                : begin + siblings.get(siblings.size() - 1).code + 1;
-
-        for (int i = 0; i < siblings.size(); i++) {
-            check[begin + siblings.get(i).code] = begin;
-        }
-
-        for (int i = 0; i < siblings.size(); i++) {
-            List<Node> newChildren = createAllChildren(siblings.get(i));
-
-            // 一个词的终止且不为其他词的前缀
-            if (newChildren.size() == 0) {
-                base[begin + siblings.get(i).code] = (values != null) ?
-                        (-values[siblings.get(i).left] - 1) : (-siblings.get(i).left - 1);
-
-                if (values != null && (-values[siblings.get(i).left] - 1) >= 0) {
-                    throw new RuntimeException("errorCode = -2 ???");
-                }
-            } else {
-                int h = insert(newChildren, used);   // dfs
-                base[begin + siblings.get(i).code] = h;
-            }
-        }
         return begin;
     }
 
     /**
-     * 拓展数组
+     * 将双数组扩展到给定的大小
      *
      * @param newSize
-     * @return
      */
-    private int resize(int newSize) {
+    private void ensureSize(int newSize) {
+        // TODO: 是否可能会存在频繁扩容的情况？是否要限制一下每次扩容的最小值。
+        if (base != null && base.length >= newSize) {
+            return;
+        }
+
         int[] newBase = new int[newSize];
         int[] newCheck = new int[newSize];
-        if (allocSize > 0) {
-            System.arraycopy(base, 0, newBase, 0, allocSize);
-            System.arraycopy(check, 0, newCheck, 0, allocSize);
+        if (base != null && base.length > 0) {
+            System.arraycopy(base, 0, newBase, 0, base.length);
+            System.arraycopy(check, 0, newCheck, 0, check.length);
         }
 
         base = newBase;
         check = newCheck;
-
-        return allocSize = newSize;
     }
 
     /**
